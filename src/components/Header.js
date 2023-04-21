262e7dd2ad260f2f2bbe7dbb6b2480f2d4461079
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-04-21 09:07:43 -0700

    Add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>Tea Tracker</h1>
+      <p>Keep track of shop inventory!</p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
