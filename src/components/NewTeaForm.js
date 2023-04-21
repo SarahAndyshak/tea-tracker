f154dd67efa0363858e946a6dc4e66a2232b6f93
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-04-21 10:25:19 -0700

    Add NewTeaForm.js

---
+++ src/components/NewTeaForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
+
+function NewTeaForm(props){
+  function handleNewTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTeaCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      caf: event.target.caf.value,
+      price: event.target.price.value,
+      inventory: event.target.inventory.value,
+      id: v4()
+    });
+  }
+
+  return(
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewTeaFormSubmission}
+        class="btn btn-light" buttonText="Add Tea" />
+    </React.Fragment>
+  );
+}
+
+NewTeaForm.propTypes = {
+  onNewTeaCreation: PropTypes.func
+};
+
+export default NewTeaForm;
