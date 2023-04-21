193bb2472f055018e4a1ef92cfd53294a34656dc
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-04-21 12:07:27 -0700

    Update functions in Tea.js

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -2,15 +2,54 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Tea(props){
-  return(
+
+  function handleClick() {
+    return props.onBuyTea(props.id);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.inventory.value);
+    return props.onRestockItem(props.id, parseInt(event.target.inventory.value));
+  }
+
+  let teaDisplay = null;
+  if (props.inventory <= 0) {
+    teaDisplay = <h4>{props.name} is <strong>Out of Stock</strong></h4>
+  } else {
+    teaDisplay = 
+    <>
+    <h3>Name: {props.name}</h3>
+    <p>Origin: {props.origin}</p>
+    <p>Caf/Decaf: {props.caf}</p>
+    <p>Price: {props.price}</p>
+    <p>Inventory: {props.inventory}</p>
+    <button className="btn btn-block btn-lg btn-dark" onClick={handleClick}>Buy</button>
+    </>
+  }
+
+  return (
     <React.Fragment>
-      <h3>Name: {props.name}</h3>
-      <p>Origin: {props.origin}</p>
-      <p>Caf/Decaf: {props.caf}</p>
-      <p>Price: {props.price}</p>
-      <p>Inventory: {props.inventory}</p>
+      <div onClick={() => props.onTeaSelect(props.id)}>
+      {teaDisplay}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="number" min="1" max="130" name="inventory" className="form-control" />
+        <button className="btn btn-outline-dark">Restock</button>
+      </form>
     </React.Fragment>
   );
+
+  //original fragment to return
+  // return(
+  //   <React.Fragment>
+  //     <h3>Name: {props.name}</h3>
+  //     <p>Origin: {props.origin}</p>
+  //     <p>Caf/Decaf: {props.caf}</p>
+  //     <p>Price: {props.price}</p>
+  //     <p>Inventory: {props.inventory}</p>
+  //   </React.Fragment>
+  // );
 }
 
 Tea.propTypes = {
@@ -18,7 +57,10 @@ Tea.propTypes = {
   origin: PropTypes.string,
   caf: PropTypes.string,
   price: PropTypes.number,
-  inventory: PropTypes.number
+  inventory: PropTypes.number,
+  id: PropTypes.string,
+  onBuyTea: PropTypes.func,
+  onTeaSelect: PropTypes.func
 }
 
 export default Tea;
